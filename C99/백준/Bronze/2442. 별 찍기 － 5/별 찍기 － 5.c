c4deb70edf5f8d1a9b4120f07f376a0554f32a5f
bf1at <sangjun726@hansung.ac.kr>
2024-08-12 15:01:06 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void) {
+	int n = 0;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		for (int j = n-i-1; j > 0; j--)
+			printf(" ");
+		for (int k = 0; k < 2*i+1; k++)
+			printf("*");
+		printf("\n");
+	}
+	return 0;
+}
