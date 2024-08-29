8e5814d272290287f1fe3f17cc1c8965607f29cb
bf1at <sangjun726@hansung.ac.kr>
2024-08-29 17:22:33 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/11021. A＋B － 7/A＋B － 7.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void) {
+	int a, b, n;
+	scanf("%d", &n);
+	for(int i = 1; i <= n; i++) {
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d\n", i,  a + b);
+	}
+	return 0;
+}
