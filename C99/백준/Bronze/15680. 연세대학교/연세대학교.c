3d1904d9e75d62173887dd2db6800e25c32f1389
bf1at <sangjun726@hansung.ac.kr>
2024-09-05 22:34:42 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	if (n == 0)
+		printf("YONSEI");
+	else
+		printf("Leading the Way to the Future");
+	return 0;
+}
