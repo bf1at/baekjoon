0fbce0bb366111d365e06a97021c3cc5fca9cbb7
bf1at <sangjun726@hansung.ac.kr>
2024-09-28 23:21:18 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char a[100] = "";
+    scanf("%99s", a);
+
+    printf(":fan::fan::fan:\n");
+    printf(":fan::%s::fan:\n", a);
+    printf(":fan::fan::fan:\n");
+
+    return 0;
+}
