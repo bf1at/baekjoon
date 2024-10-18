1eae67efd11f895c30e9e0d675239931acf96e90
bf1at <sangjun726@hansung.ac.kr>
2024-10-18 13:21:05 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

---
+++ C99/백준/Bronze/21598. SciComLove/SciComLove.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a;
+	scanf("%d", &a);
+	if (a == 1)
+		printf("SciComLove");
+	else
+		printf("SciComLove\nSciComLove");
+	return 0;
+}
