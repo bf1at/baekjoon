64295bc27e00b1506d341cc731a48bb12088ebb5
bf1at <sangjun726@hansung.ac.kr>
2024-10-26 02:47:37 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/24086. 身長 （Height）/身長 （Height）.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(){
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", b - a);
+	return 0;
+}
