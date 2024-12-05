93c4504948daa060f397c93b05ba28749ff733b9
bf1at <sangjun726@hansung.ac.kr>
2024-12-05 18:18:51 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	char a[20];
+	scanf("%s", a);
+	if (strcmp(a, "SONGDO") == 0)
+		printf("HIGHSCHOOL");
+	else if (strcmp(a, "CODE") == 0)
+		printf("MASTER");
+	else if (strcmp(a, "2023") == 0)
+		printf("0611");
+	else if (strcmp(a, "ALGORITHM") == 0)
+		printf("CONTEST");
+	return 0;
+}
