ba923233b6aaeafbe1527634e057679281b94a00
bf1at <sangjun726@hansung.ac.kr>
2025-01-03 13:27:54 +0900

    [Bronze III] Title: TGN, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, n;
+    scanf("%d",&n);
+    while(n--){
+        scanf("%d %d %d",&a,&b,&c);
+        if((b-a)>c)
+            printf("advertise\n");
+        else if((b-a)==c)
+            printf("does not matter\n");
+        else
+            printf("do not advertise\n");
+    }
+    return 0;
+}
