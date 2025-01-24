b2fb969ab12b62ae7800640239eab0c2385c964f
bf1at <sangjun726@hansung.ac.kr>
2025-01-24 16:58:56 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    long long a,b,c;
+    scanf("%lld %lld %lld",&a,&b,&c);
+    if(a+b==c)
+        printf("correct!");
+    else
+        printf("wrong!");
+    return 0;
+}
