9a4651d5164cb4077c3a1a6801b38a669666e8a7
bf1at <sangjun726@hansung.ac.kr>
2025-02-25 09:00:32 +0900

    [Bronze III] Title: What is n, Daddy?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/6794. What is n， Daddy？/What is n， Daddy？.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n==1)
+        printf("1");
+    else if(n==2)
+        printf("2");
+    else if(n==3)
+        printf("2");
+    else if(n==4)
+        printf("3");
+    else if(n==5)
+        printf("3");
+    else if(n==6)
+        printf("3");
+    else if(n==7)
+        printf("2");
+    else if(n==8)
+        printf("2");
+    else if(n==9)
+        printf("1");
+    else if(n==10)
+        printf("1");
+    return 0;
+}
