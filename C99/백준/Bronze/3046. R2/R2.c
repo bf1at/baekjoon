08e599e66b745849529f18e71c4606865c7172b8
bf1at <sangjun726@hansung.ac.kr>
2025-03-07 13:23:21 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/3046. R2/R2.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(){
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", b * 2 - a);
+	return 0;
+}
