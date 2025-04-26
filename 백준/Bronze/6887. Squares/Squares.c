bfdf693ad56e5d4e52146a66bfd7e79119e9e6d2
bf1at <sangjun726@hansung.ac.kr>
2025-04-26 15:15:08 +0900

    [Bronze IV] Title: Squares, Time: 0 ms, Memory: 1128 KB -BaekjoonHub

---
+++ 백준/Bronze/6887. Squares/Squares.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include <math.h>
+
+int main() {
+
+    int n;
+
+    scanf("%d",&n);
+
+    printf("The largest square has side length %d.",(int)sqrt((double)n));
+
+    return 0;
+
+}
