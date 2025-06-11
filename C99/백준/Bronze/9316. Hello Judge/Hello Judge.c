845dfb5cfe7fd4b28d24f1475c2a85d0a962936d
bf1at <sangjun726@hansung.ac.kr>
2025-06-11 19:32:09 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/9316. Hello Judge/Hello Judge.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		printf("Hello World, Judge %d!\n", i);
+	}
+	return 0;
+} 
