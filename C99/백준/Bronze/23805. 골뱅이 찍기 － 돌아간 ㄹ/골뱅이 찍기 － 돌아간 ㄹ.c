b2bfdbca226b17736d1c4aad60930f498861e176
bf1at <sangjun726@hansung.ac.kr>
2025-07-28 00:17:00 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 4 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++){
+
+        for(int j=0;j<n*3;j++)
+
+            printf("@");
+
+        for(int j=0;j<n;j++)
+
+            printf(" ");
+
+        for(int j=0;j<n;j++)
+
+            printf("@");
+
+        printf("\n");
+
+    }
+
+    for(int i=0;i<n*3;i++){
+
+        for(int j=0;j<n;j++)
+
+            printf("@");
+
+        for(int j=0;j<n;j++)
+
+            printf(" ");
+
+        for(int j=0;j<n;j++)
+
+            printf("@");
+
+        for(int j=0;j<n;j++)
+
+            printf(" ");
+
+        for(int j=0;j<n;j++)
+
+            printf("@");
+
+        printf("\n");
+
+    }
+
+    for(int i=0;i<n;i++){
+
+        for(int j=0;j<n;j++)
+
+            printf("@");
+
+        for(int j=0;j<n;j++)
+
+            printf(" ");
+
+        for(int j=0;j<n*3;j++)
+
+            printf("@");
+
+        printf("\n");
+
+    }
+
+    return 0;
+
+}
