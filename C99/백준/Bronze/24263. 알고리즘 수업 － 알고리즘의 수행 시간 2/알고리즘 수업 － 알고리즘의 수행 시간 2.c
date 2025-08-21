8907972a71411788c5106114f8a80bf291236d9d
bf1at <sangjun726@hansung.ac.kr>
2025-08-22 00:03:21 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main() {
+    long long n;
+    scanf("%lld", &n);
+    printf("%lld\n1", n);
+    return 0;
+}
