22a58082b118f3b422503ff8827842f304fdb063
bf1at <sangjun726@hansung.ac.kr>
2025-08-26 12:20:35 +0900

    [Bronze III] Title: 분수좋아해?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/10474. 분수좋아해？/분수좋아해？.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    long long a,b;
+
+    while(1){
+
+        scanf("%lld %lld",&a,&b);
+
+        if(a==0&&b==0)
+
+            break;
+
+        printf("%lld %lld / %lld\n",a/b,a%b,b);
+
+    }
+
+    return 0;
+
+}
