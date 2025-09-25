95dfb21bcdd6bbfce30d88e332d6045986750b61
bf1at <sangjun726@hansung.ac.kr>
2025-09-26 00:49:11 +0900

    [Bronze III] Title: 성장의 비약 선택권, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/26645. 성장의 비약 선택권/성장의 비약 선택권.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+
+    scanf("%d",&n);
+
+    if(n<=205)
+
+        printf("1");
+
+    else if(n<=217)
+
+        printf("2");
+
+    else if(n<=228)
+
+        printf("3");
+
+    else
+
+        printf("4");
+
+    return 0;
+
+}
