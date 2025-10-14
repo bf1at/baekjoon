d77a9415c4abde19c16526e95309b840f5dc1562
bf1at <sangjun726@hansung.ac.kr>
2025-10-14 20:43:50 +0900

    [Bronze IV] Title: Take It or Double It, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/34584. Take It or Double It/Take It or Double It.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b;
+
+    scanf("%d %d",&a,&b);
+
+    if(a*2>b)
+
+        printf("take it");
+
+    else
+
+        printf("double it");
+
+    return 0;
+
+}
