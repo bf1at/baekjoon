bf671a01a37bd25ce2af6d9e6a2f849151458c21
bf1at <sangjun726@hansung.ac.kr>
2025-12-09 20:24:12 +0900

    [Bronze IV] Title: 이 대회는 이제 제 겁니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b,c;
+
+    scanf("%d %d %d",&a,&b,&c);
+
+    if(a+c>b)
+
+        printf("%d",a+c);
+
+    else
+
+        printf("%d",b);
+
+    return 0;
+
+}
