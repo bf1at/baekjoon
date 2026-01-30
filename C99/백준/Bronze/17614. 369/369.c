84d1c94d23670b44709ee1d277677aed9bf2b322
bf1at <sangjun726@hansung.ac.kr>
2026-01-30 15:18:01 +0900

    [Bronze III] Title: 369, Time: 0 ms, Memory: 1116 KB, Score: 4 point -BaekjoonHub

---
+++ C99/백준/Bronze/17614. 369/369.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+int main() {
+	int n, sum = 0;
+	char s[10];
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		sprintf(s, "%d", i);
+		for (int j = 0; j < strlen(s); j++) {
+			if (s[j] == '3' || s[j] == '6' || s[j] == '7')
+				sum++;
+		}
+	}
+	printf("%d", sum);
+
+	return 0;
+}
